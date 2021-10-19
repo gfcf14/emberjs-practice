7f4c26ca95f4813c6683e104fbea80929326dcea
gfcf14 <carloscuba014@gmail.com>
2021-10-19 17:08:57 -0400

    controllers and actions

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,6 +1,21 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class PostController extends Controller {
+  sayHello() {
+    alert('hello');
+  }
+
+  @tracked name;
+  @tracked comment;
+
+  @action
+  submitAction(ev) {
+    ev.preventDefault();
+    alert(`${this.name}: ${this.comment}`);
+  }
+
   title = 'My Blog Post';
   body = 'This is the body of my blog post';
   authors = ['William', 'Robert', 'Michelle'];
